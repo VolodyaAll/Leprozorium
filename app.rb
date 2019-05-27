5cc6e1b300ad7fcfa33313ee07fd569d6f002ea1
All <volodya22all@gmail.com>
2019-05-27 15:24:12 +0300

    Add detales view

--- app.rb
+++ app.rb
@@ -47,5 +47,10 @@ end
 
 get '/detales/:post_id' do
 	post_id = params[:post_id]
-	erb "Info for #{post_id}"
+
+	results = @db.execute 'select * from Posts where id=?', [post_id]
+	
+	@row = results[0]
+
+	erb :detales
 end
